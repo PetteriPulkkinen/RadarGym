249059c33cee6c7fc3caddaa362b4dcdc420b3e4
pulkkip6 <petteri.pulkkinen@aalto.fi>
2020-04-29 10:49:58 +0300

    Generalize discrete revisit interval environment
    Signed-off-by: pulkkip6 <petteri.pulkkinen@aalto.fi>

--- radargym/__init__.py
+++ radargym/__init__.py
@@ -1,4 +1,6 @@
 from gym.envs.registration import register
+from trackingsimpy.simulation.revisit_interval import DefinedIMM, BaselineKalman
+
 
 register(
     id='Tracking-v0',
@@ -11,11 +13,31 @@ register(
 )
 
 register(
-    id='revisit-v0',
-    entry_point='radargym.single_target_tracking:RevisitIntervalDiscrete'
+    id='revisit-v00',
+    entry_point='radargym.single_target_tracking:RevisitIntervalDiscrete',
+    kwargs={
+        'sim': BaselineKalman(traj_idx=4, var=1e3),
+        'p_loss': 5000,
+        'n_obs': 10,
+        'n_act': 10,
+        'g_low': 0.1,
+        'g_high': 1.0,
+        'ri_min': 1,
+        'ri_max': 100,
+    }
 )
 
 register(
-    id='revisit-v1',
-    entry_point='radargym.single_target_tracking:RevisitIntervalContinuous'
+    id='revisit-v01',
+    entry_point='radargym.single_target_tracking:RevisitIntervalDiscrete',
+    kwargs={
+        'sim': DefinedIMM(),
+        'p_loss': 5000,
+        'n_obs': 10,
+        'n_act': 10,
+        'g_low': 0.1,
+        'g_high': 1.0,
+        'ri_min': 1,
+        'ri_max': 250,
+    }
 )
--- radargym/single_target_tracking/revisit_intervals.py
+++ radargym/single_target_tracking/revisit_intervals.py
@@ -1,137 +1,37 @@
 import gym
 from gym import spaces
-from trackingsimpy.simulation.revisit_interval import BaselineKalman
 from trackingsimpy.common.trigonometrics import pos_to_angle_error_2D
 import numpy as np
-import matplotlib.pyplot as plt
-
-
-class Renderer(object):
-    """Renders target, trajectory, radar, measurements and predictions.
-    """
-
-    def __init__(self, simulation):
-        self.figure = None
-        self.ax = None  # scene
-        self.ax_ri = None  # revisit interval
-        self.ax_ae = None  # angular error
-        self.target = None
-        self.measurement = None
-        self.prediction = None
-        self.intervals = None
-        self.theta_errors = None
-        self.sim = simulation
-        self.is_initialized = False
-
-    def initialize(self):
-        self.figure, axes = plt.subplots(3, 1)
-        self.ax = axes[0]
-        self.ax_ri = axes[1]
-        self.ax_ae = axes[2]
-        self.flush()
-        plt.show(block=False)
-        self.is_initialized = True
-
-    def flush(self):
-        self.ax.cla()
-        self.ax_ri.cla()
-        self.ax_ae.cla()
-
-        self.ax.grid(True)
-        self.ax_ri.grid(True)
-        self.ax_ae.grid(True)
-
-        self.ax.plot([0], [0], 'o', label='radar')
-        self.ax.plot(self.sim.target.trajectory[:, 0], self.sim.target.trajectory[:, 2], '--', label='trajectory')
-
-        self.intervals = self.ax_ri.plot(1, '-o')[0]
-
-        self.theta_errors = self.ax_ae.plot(0, '-o')[0]
-
-        pos = self._target_pos()
-        pos_prior = self._prediction_pos()
-
-        self.target = self.ax.plot(pos[0], pos[1], '*', label='target')[0]
-        self.measurement = self.ax.plot(pos[0], pos[1], 'x', label='measurement')[0]  # No measurements at the beginning
-        self.prediction = self.ax.plot(pos_prior[0], pos_prior[1], 'o', label='prediction')[0]
-
-        self.ax.legend()
-
-    def update(self, revisit_interval, angular_error):
-        if not self.is_initialized:
-            self.initialize()
-        else:
-            pos = self._target_pos()
-            pos_prior = self._prediction_pos()
-            z = self._measurement_pos()
-
-            self.target.set_data(pos[0], pos[1])
-            self.measurement.set_data(z[0], z[1])
-            self.prediction.set_data(pos_prior[0], pos_prior[1])
-
-            (x, y) = self.intervals.get_data()
-
-            x = np.append(x, x.max()+1)
-            y = np.append(y, revisit_interval)
-            self.intervals.set_data(x, y)
-            self.ax_ri.relim()
-            self.ax_ri.autoscale_view()
-
-            (x, y) = self.theta_errors.get_data()
-
-            x = np.append(x, x.max() + 1)
-            y = np.append(y, angular_error)
-            self.theta_errors.set_data(x, y)
-            self.ax_ae.relim()
-            self.ax_ae.autoscale_view()
-
-    def _target_pos(self):
-        return self.sim.target.position
-
-    def _prediction_pos(self):
-        return self.sim.tracker.x_prior.flatten()[[0, 2]]
-
-    def _measurement_pos(self):
-        return self.sim.computer.z.flatten()
 
 
 class RevisitIntervalDiscrete(gym.Env):
-    RI_MAX = 100
-    RI_MIN = 1
-    N_ACTS = 10
-    N_OBS = 10
-    C_LOW = 0.1  # lower limit multiplier for beamwidth
-    C_HIGH = 1  # higher limit multiplier for beamwidth
-    P_LOSS = 5000
+    def __init__(self, sim=None, p_loss=5000, ri_min=1, ri_max=100, n_act=10, n_obs=10, g_low=0.1, g_high=1):
+        self.sim = sim
+        self.p_loss = p_loss
+        self.ri_min = ri_min
+        self.ri_max = ri_max
+        self.n_act = n_act
+        self.n_obs = n_obs
+        self.g_low = g_low
+        self.g_high = g_high
 
-    def __init__(self):
-        self.sim = BaselineKalman(
-            traj_idx=4,
-            var=1000,
-            k_min=self.RI_MIN,
-            k_max=self.RI_MAX
-        )
         self.revisit_interval = 1
         self.angle_error = 0
 
-        self.observation_space = spaces.Discrete(self.N_OBS)  # Discretized theta values
-        self.action_space = spaces.Discrete(self.N_ACTS)  # Discretized revisit interval values
-
-        self.renderer = Renderer(self.sim)
+        self.observation_space = spaces.Discrete(self.n_obs)  # Discretized theta values
+        self.action_space = spaces.Discrete(self.n_act)  # Discretized revisit interval values
 
         super().__init__()
 
     def reset(self):
-        if self.renderer.is_initialized:
-            self.renderer.flush()
         self.sim.reset()
         return self._observation(update_successful=True)
 
     def step(self, action):
         self.revisit_interval = self.action2ri(action)
-        update_successful, n_missed, trajectory_ends = self.sim.step(self.revisit_interval)
+        update_successful, _, trajectory_ends = self.sim.step(self.revisit_interval)
         obs = self._observation(update_successful)
-        reward = self._reward(update_successful, n_missed, self.revisit_interval)
+        reward = self._reward(update_successful, self.revisit_interval)
 
         if trajectory_ends or not update_successful:
             done = True
@@ -140,112 +40,44 @@ class RevisitIntervalDiscrete(gym.Env):
         return obs, reward, done, {}
 
     def render(self, mode='human'):
-        self.renderer.update(self.revisit_interval, self.angle_error)
-        self.renderer.figure.canvas.draw()
+        pass
 
     def _observation(self, update_successful):
         if update_successful:
             pos = self.sim.target.position
-            pos_est = self.sim.tracker.H @ self.sim.tracker.x.flatten()
+            pos_est = self.sim.radar.H @ self.sim.tracker.x.flatten()
 
             self.angle_error = pos_to_angle_error_2D(pos, pos_est)
             return self._discretize(self.angle_error, is_lost=False)
         else:
             return self._discretize(np.pi, is_lost=True)
 
-    def _reward(self, update_successful, n_missed, revisit_interval):
+    def _reward(self, update_successful, revisit_interval):
         if update_successful:
-            #  return - (n_missed + 1) / (revisit_interval * self.sim.computer.n_max)
             return - 1 / revisit_interval
         else:
-            return - self.P_LOSS
+            return - self.p_loss
 
     def action2ri(self, action):
-        return int((action * self.RI_MAX + (self.N_ACTS - action - 1) * self.RI_MIN) / (self.N_ACTS - 1))
+        return int((action * self.ri_max + (self.n_act - action - 1) * self.ri_min) / (self.n_act - 1))
 
     def _discretize(self, theta, is_lost):
-        b_low = self.C_LOW * self.sim.radar.beamwidth
-        b_high = self.C_HIGH * self.sim.radar.beamwidth
+        b_low = self.g_low * self.sim.radar.beamwidth
+        b_high = self.g_high * self.sim.radar.beamwidth
 
         if is_lost:
-            return self.N_OBS - 1
+            return self.n_obs - 1
 
-        n = np.arange(0, self.N_OBS - 2)
+        n = np.arange(0, self.n_obs - 2)
         b = b_low
-        a = (b_high - b_low) / (self.N_OBS - 3)
+        a = (b_high - b_low) / (self.n_obs - 3)
 
         f = a * n + b
 
         booleans = (f >= theta)
-        if booleans.sum() == self.N_OBS - 2:
+        if booleans.sum() == self.n_obs - 2:
             return 0
         elif booleans.sum() == 0:
-            return self.N_OBS - 2
+            return self.n_obs - 2
         else:
             return np.argmin(booleans == False)
-
-
-class RevisitIntervalContinuous(gym.Env):
-    RI_MAX = 100
-    RI_MIN = 1
-    N_ACTS = 10
-
-    def __init__(self):
-        self.sim = BaselineKalman(
-            save=False,
-            traj_idx=4,
-            var=1000,
-            k_min=self.RI_MIN,
-            k_max=self.RI_MAX
-        )
-
-        self.revisit_interval = 1
-
-        self.observation_space = spaces.Box(low=0, high=np.pi, shape=(1,), dtype=np.float)  # Angle and radius delta
-        self.action_space = spaces.Discrete(self.N_ACTS)  # Discretized revisit interval values
-
-        self.renderer = Renderer(self.sim)
-
-        super().__init__()
-
-    def reset(self):
-        if self.renderer.is_initialized:
-            self.renderer.flush()
-        obs = self.sim.reset()
-        return self._observation(update_successful=True)
-
-    def step(self, action):
-        self.revisit_interval = self._action2ri(action)
-        update_successful, n_missed, trajectory_ends = self.sim.step(self.revisit_interval)
-        obs = self._observation(update_successful)
-        reward = self._reward(update_successful, n_missed, self.revisit_interval)
-
-        if trajectory_ends or not update_successful:
-            done = True
-        else:
-            done = False
-        return obs, reward, done, {}
-
-    def render(self, mode='human'):
-        self.renderer.update()
-        self.renderer.figure.canvas.draw()
-
-    def _observation(self, update_successful):
-        if update_successful:
-            H = self.sim.tracker_computer.tracker.H
-            pos = H @ self.sim.target.x.flatten()
-            pos_est = H @ self.sim.tracker_computer.tracker.x.flatten()
-
-            angle_error = pos_to_angle_error_2D(pos, pos_est)
-            return np.array([angle_error])
-        else:
-            return np.array([np.pi])
-
-    def _reward(self, update_successful, n_missed, revisit_interval):
-        if update_successful:
-            return - (n_missed + 1) / revisit_interval
-        else:
-            return - self.sim.p_loss
-
-    def _action2ri(self, action):
-        return int((action * self.RI_MAX + (self.N_ACTS - action - 1) * self.RI_MIN) / (self.N_ACTS - 1))
